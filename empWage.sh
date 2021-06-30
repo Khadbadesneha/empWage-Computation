19ee89f9d782dec4a6502df5e1e3632a2aa2dd51
Khadbadesneha <86665153+Khadbadesneha@users.noreply.github.com>
2021-06-30 20:52:09 +0530

    Added: function to find the working hours

--- empWage.sh
+++ empWage.sh
@@ -3,18 +3,19 @@
 IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=0
 empWagePerHrs=20
-empDaysMonth=20
-empCheck=$((RANDOM%3))
 
-case $empCheck in
-			$IS_PRESENT_FULL_TIME)
-										empHrs=8
-										;;
-			$IS_PRESENT_HALF_TIME)
-										empHrs=4
-										;;
-			                    *)
-										empHrs=0
-										;;
-esac
-salary=$(( $empHrs*$empWagePerHrs*$empDaysMonth ))
+empCheck=$((RANDOM%3))
+function getworkingHrs() {
+	if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
+	then
+		empHrs=8	
+	elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
+	then
+		empHrs=4
+	else
+		empHrs=0
+	fi
+echo $empHrs
+}
+
+workingHours="$(getworkingHrs $empCheck)"
