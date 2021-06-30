a444f21779931fbe7edd3cd13508133a8b854c6e
Khadbadesneha <86665153+Khadbadesneha@users.noreply.github.com>
2021-06-30 19:50:51 +0530

    Added: checking part or full time using switch statement also calculate wage

--- empWage.sh
+++ empWage.sh
@@ -4,13 +4,15 @@ IS_PRESENT_FULL_TIME=1
 IS_PRESENT_HALF_TIME=0
 empWagePerHrs=20
 empCheck=$((RANDOM%3))
-if [ $empCheck -eq $IS_PRESENT_FULL_TIME ]
-then
-	empHrs=8
-elif [ $empCheck -eq $IS_PRESENT_HALF_TIME ]
-then
-	empHrs=4
-else
-	empHrs=0
-fi
+case $empCheck in
+			$IS_PRESENT_FULL_TIME)
+										empHrs=8
+										;;
+			$IS_PRESENT_HALF_TIME)
+										empHrs=4
+										;;
+			                    *)
+										empHrs=0
+										;;
+esac
 salary=$(($empHrs*$empWagePerHrs))
